c7d27e07c69577966784171f8cfa18602581d141
Milad <milad4361@gmail.com>
2018-03-31 16:46:11 +0200

    Verbesserung

--- src/main/java/ueb01/CharStackImpl.java
+++ src/main/java/ueb01/CharStackImpl.java
@@ -1,4 +1,48 @@
 package ueb01;
 
+import java.util.NoSuchElementException;
+
 class CharStackImpl implements CharStack {
+    private Element top;
+    private int counterPush = 0;
+    private int counterPop = 0;
+
+    @Override
+    public void push(char c) {
+
+        if (top == null){
+        top = new Element(c, null);
+        counterPush++;
+        return;
+
+        } else{
+            Element copyTop = top;
+            top = new Element(c,copyTop );
+            counterPush++;
+        }
+
+
+
+    }
+
+    @Override
+    public char pop() {
+        Element copyTop;
+        if (top == null){
+            throw new NoSuchElementException();
+        }else {
+            copyTop = top;
+            top = top.next;
+            counterPop++;
+
+        }
+
+        return copyTop.value;
+
+    }
+
+    @Override
+    public int size() {
+        return (counterPush - counterPop);
+    }
 }
---
+++ src/main/java/ueb01/Element.java
@@ -0,0 +1,13 @@
+package ueb01;
+
+public class Element {
+    char value;
+    Element next;
+
+    Element(char value, Element next){
+        this.value = value;
+        this.next = next;
+    }
+
+
+}
